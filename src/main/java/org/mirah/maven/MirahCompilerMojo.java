f0f68c229e9983e5dc926b78a7a49e6cc2720ecd
David Calavera <david.calavera@gmail.com>
2010-12-02 19:28:54 +0100

    use project classpath to resolve mirah dependencies

--- src/main/java/org/mirah/maven/MirahCompilerMojo.java
+++ src/main/java/org/mirah/maven/MirahCompilerMojo.java
@@ -1,23 +1,8 @@
 package org.mirah.maven;
 
-/*
- * Copyright 2001-2005 The Apache Software Foundation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.project.MavenProject;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -26,6 +11,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.codehaus.plexus.util.StringUtils;
+
 import org.mirah.MirahCommand;
 
 /**
@@ -33,9 +20,15 @@ import org.mirah.MirahCommand;
  *
  * @goal compile
  * @phase compile
- * @requiresDependencyResolution compile
+ * @requiresDependencyResolution
  */
 public class MirahCompilerMojo extends AbstractMojo {
+    /**
+     * @parameter expression="${project}"
+     * @required
+     * @readonly
+     */
+    private MavenProject project;
     /**
      * Classes destination directory
      * @parameter expression="${project.build.outputDirectory}"
@@ -71,9 +64,16 @@ public class MirahCompilerMojo extends AbstractMojo {
         arguments.add("-d");
         arguments.add(outputDirectory.getAbsolutePath());
 
-        arguments.add(sourceDirectory.getAbsolutePath());
-
         try {
+            List<String> compileClasspath = project.getCompileClasspathElements();
+            compileClasspath.addAll(project.getRuntimeClasspathElements());
+            String classpath = StringUtils.join(compileClasspath.iterator(), File.pathSeparator);
+
+            arguments.add("-c");
+            arguments.add(classpath);
+
+            arguments.add(sourceDirectory.getAbsolutePath());
+
             MirahCommand.compile(arguments);
         } catch (Exception e) {
             throw new MojoExecutionException(e.getMessage(), e);
